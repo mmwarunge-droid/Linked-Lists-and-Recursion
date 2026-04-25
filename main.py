f4bf50057f45bb55ffc62f965f09e918093a5302
Mark Warunge <mmwarunge@gmail.com>
2026-04-25 13:42:17 +0300

    Initial commit

--- main.py
+++ main.py
@@ -7,20 +7,28 @@ if __name__ == "__main__":
     """
 
     # TODO: 1) Create a LinkedList instance
+    ll = LinkedList()
     
 
     # TODO: 2) Insert some sample data using insert_at_front or insert_at_end
-    
+    ll.insert_at_end(1)
+    ll.insert_at_end(2)
+    ll.insert_at_end(3)
+
     # TODO: 3) Display the list to verify insertion
-    
+    ll.display()
 
     # TODO: 4) Call recursive_sum and print the result
-    
+    print("Sum:", ll.recursive_sum())
 
     # TODO: 5) Call recursive_search with a target and print result
-    
+    print("Search 2:", ll.recursive_search(2))
+    print("Search 4:", ll.recursive_search(4))
 
     # TODO: 6) Call recursive_reverse, then display the reversed list
+    ll.recursive_reverse()
+    ll.display()
+
     
 
 
